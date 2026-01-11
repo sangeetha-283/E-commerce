54ccffb740e253eb1a0054fafdd2f617ffbd69d4
Sangeetha Kumar <sagie.poorni@gmail.com>
2026-01-11 15:57:29 +0530

    Vercel router fix

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app.vercel.app"
+      "https://e-commerce-psi-beige.vercel.app/"
     ],
     credentials: true,
   })
